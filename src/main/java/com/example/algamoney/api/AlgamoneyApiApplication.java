df974da2c88c91f543adf0b95326d4092ccb4c6d
BrunoCanfora <bruno.canfora@gmail.com>
2018-01-25 22:27:44 -0200

    Consultando o primeiro recurso com GET(Teste via postman)

--- src/main/java/com/example/algamoney/api/AlgamoneyApiApplication.java
+++ src/main/java/com/example/algamoney/api/AlgamoneyApiApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AlgamoneyApiApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Teste");
 		SpringApplication.run(AlgamoneyApiApplication.class, args);
 	}
 }
